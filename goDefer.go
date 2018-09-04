6f8566c447744346dface8fafa70fee8cb549513
zhangzhiwei <zhangzhiwei@trioly.com>
2018-09-04 17:07:28 +0800

    json panic goroutine

--- goDefer.go
+++ goDefer.go
@@ -4,7 +4,7 @@ import "fmt"
 import "os"
 
 // 假设我们想创建一个文件，然后写入数据，最后关闭文件
-func main() {
+func main() { //使用defer来调用closeFile函数可以保证在main函数结束之前，关闭文件的操作一定会被执行
 	// 在使用createFile得到一个文件对象之后，我们使用defer
 	// 来调用关闭文件的方法closeFile，这个方法将在main函数
 	// 最后被执行，也就是writeFile完成之后
